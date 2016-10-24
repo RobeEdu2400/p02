eed6c7df334b49a16d6bd9195e66d05957a82232
Roberto <robe_edu@hotmail.com>
2016-10-24 19:30:22 +0200

    p02

--- p02/p02/Form1.vb
+++ p02/p02/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
+    Private Sub Ordenación_Click(sender As Object, e As EventArgs) Handles Ordenación.Click
+        Try
+            TextBox1.Text = CInt(Text)
 
+        Catch ex As Exception
+            MsgBox()
+        End Try
+    End Sub
 End Class
