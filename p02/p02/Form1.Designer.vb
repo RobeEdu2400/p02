79dcc1b13ccf70d482bf56111c281569812e87cd
Roberto <robe_edu@hotmail.com>
2016-10-30 18:04:56 +0100

    update

--- p02/p02/Form1.Designer.vb
+++ p02/p02/Form1.Designer.vb
@@ -47,24 +47,28 @@ Partial Class Form1
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Ordenación
         '
-        Me.Ordenación.Location = New System.Drawing.Point(309, 56)
+        Me.Ordenación.Location = New System.Drawing.Point(328, 73)
         Me.Ordenación.Name = "Ordenación"
         Me.Ordenación.Size = New System.Drawing.Size(113, 39)
-        Me.Ordenación.TabIndex = 8
+        Me.Ordenación.TabIndex = 9
         Me.Ordenación.Text = "Ordenación"
         Me.Ordenación.UseVisualStyleBackColor = True
         '
         'RadioButton1
         '
         Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(309, 101)
+        Me.RadioButton1.Location = New System.Drawing.Point(447, 72)
         Me.RadioButton1.Name = "RadioButton1"
         Me.RadioButton1.Size = New System.Drawing.Size(104, 21)
-        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabIndex = 10
         Me.RadioButton1.TabStop = True
         Me.RadioButton1.Text = "Ascendente"
         Me.RadioButton1.UseVisualStyleBackColor = True
@@ -72,122 +76,122 @@ Partial Class Form1
         'RadioButton2
         '
         Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(309, 141)
+        Me.RadioButton2.Location = New System.Drawing.Point(447, 97)
         Me.RadioButton2.Name = "RadioButton2"
         Me.RadioButton2.Size = New System.Drawing.Size(113, 21)
-        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabIndex = 11
         Me.RadioButton2.TabStop = True
         Me.RadioButton2.Text = "Descendente"
         Me.RadioButton2.UseVisualStyleBackColor = True
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(59, 101)
+        Me.TextBox2.Location = New System.Drawing.Point(92, 101)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(148, 22)
-        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.TabIndex = 2
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(59, 130)
+        Me.TextBox3.Location = New System.Drawing.Point(92, 130)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(148, 22)
-        Me.TextBox3.TabIndex = 2
+        Me.TextBox3.TabIndex = 3
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(59, 73)
+        Me.TextBox1.Location = New System.Drawing.Point(92, 73)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(148, 22)
-        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TabIndex = 1
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(59, 159)
+        Me.TextBox4.Location = New System.Drawing.Point(92, 159)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(148, 22)
-        Me.TextBox4.TabIndex = 3
+        Me.TextBox4.TabIndex = 4
         '
         'TextBox5
         '
-        Me.TextBox5.Location = New System.Drawing.Point(59, 188)
+        Me.TextBox5.Location = New System.Drawing.Point(92, 188)
         Me.TextBox5.Name = "TextBox5"
         Me.TextBox5.Size = New System.Drawing.Size(148, 22)
-        Me.TextBox5.TabIndex = 4
+        Me.TextBox5.TabIndex = 5
         '
         'TextBox6
         '
-        Me.TextBox6.Location = New System.Drawing.Point(59, 217)
+        Me.TextBox6.Location = New System.Drawing.Point(92, 217)
         Me.TextBox6.Name = "TextBox6"
         Me.TextBox6.Size = New System.Drawing.Size(148, 22)
-        Me.TextBox6.TabIndex = 5
+        Me.TextBox6.TabIndex = 6
         '
         'TextBox7
         '
-        Me.TextBox7.Location = New System.Drawing.Point(59, 246)
+        Me.TextBox7.Location = New System.Drawing.Point(92, 246)
         Me.TextBox7.Name = "TextBox7"
         Me.TextBox7.Size = New System.Drawing.Size(148, 22)
-        Me.TextBox7.TabIndex = 6
+        Me.TextBox7.TabIndex = 7
         '
         'TextBox8
         '
-        Me.TextBox8.Location = New System.Drawing.Point(59, 275)
+        Me.TextBox8.Location = New System.Drawing.Point(92, 275)
         Me.TextBox8.Name = "TextBox8"
         Me.TextBox8.Size = New System.Drawing.Size(148, 22)
-        Me.TextBox8.TabIndex = 7
+        Me.TextBox8.TabIndex = 8
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(383, 183)
+        Me.Button1.Location = New System.Drawing.Point(447, 188)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(100, 22)
-        Me.Button1.TabIndex = 12
-        Me.Button1.Text = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 33)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Divisibles"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox9
         '
-        Me.TextBox9.Location = New System.Drawing.Point(277, 183)
+        Me.TextBox9.Location = New System.Drawing.Point(328, 188)
         Me.TextBox9.Name = "TextBox9"
         Me.TextBox9.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox9.TabIndex = 11
+        Me.TextBox9.TabIndex = 12
         '
         'TextBox10
         '
-        Me.TextBox10.Location = New System.Drawing.Point(277, 246)
+        Me.TextBox10.Location = New System.Drawing.Point(328, 246)
         Me.TextBox10.Name = "TextBox10"
         Me.TextBox10.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox10.TabIndex = 13
+        Me.TextBox10.TabIndex = 14
         '
         'TextBox11
         '
-        Me.TextBox11.Location = New System.Drawing.Point(277, 275)
+        Me.TextBox11.Location = New System.Drawing.Point(328, 274)
         Me.TextBox11.Name = "TextBox11"
         Me.TextBox11.Size = New System.Drawing.Size(100, 22)
         Me.TextBox11.TabIndex = 15
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(383, 246)
+        Me.Button3.Location = New System.Drawing.Point(447, 246)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(100, 22)
-        Me.Button3.TabIndex = 14
-        Me.Button3.Text = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 31)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Ultimo Día"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(59, 50)
+        Me.Label1.Location = New System.Drawing.Point(92, 50)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(148, 17)
-        Me.Label1.TabIndex = 16
+        Me.Label1.TabIndex = 0
         Me.Label1.Text = "Introduce los números"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(14, 73)
+        Me.Label2.Location = New System.Drawing.Point(47, 73)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(39, 17)
         Me.Label2.TabIndex = 17
@@ -196,7 +200,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(14, 101)
+        Me.Label3.Location = New System.Drawing.Point(47, 101)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(39, 17)
         Me.Label3.TabIndex = 18
@@ -205,7 +209,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(14, 130)
+        Me.Label4.Location = New System.Drawing.Point(47, 130)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(39, 17)
         Me.Label4.TabIndex = 19
@@ -214,7 +218,7 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(14, 159)
+        Me.Label5.Location = New System.Drawing.Point(47, 159)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(39, 17)
         Me.Label5.TabIndex = 20
@@ -223,7 +227,7 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(14, 188)
+        Me.Label6.Location = New System.Drawing.Point(47, 188)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(39, 17)
         Me.Label6.TabIndex = 21
@@ -232,7 +236,7 @@ Partial Class Form1
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(14, 217)
+        Me.Label7.Location = New System.Drawing.Point(47, 217)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(39, 17)
         Me.Label7.TabIndex = 22
@@ -241,7 +245,7 @@ Partial Class Form1
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(14, 246)
+        Me.Label8.Location = New System.Drawing.Point(47, 246)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(39, 17)
         Me.Label8.TabIndex = 23
@@ -250,17 +254,58 @@ Partial Class Form1
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(14, 275)
+        Me.Label9.Location = New System.Drawing.Point(47, 275)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(39, 17)
         Me.Label9.TabIndex = 24
         Me.Label9.Text = "N º 8"
         '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label10.Location = New System.Drawing.Point(325, 308)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(59, 17)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "Label10"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(325, 159)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(66, 17)
+        Me.Label11.TabIndex = 25
+        Me.Label11.Text = "Divisores"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(291, 246)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(34, 17)
+        Me.Label12.TabIndex = 26
+        Me.Label12.Text = "Mes"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(291, 274)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(33, 17)
+        Me.Label13.TabIndex = 27
+        Me.Label13.Text = "Año"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(525, 345)
+        Me.ClientSize = New System.Drawing.Size(599, 343)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
         Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
@@ -286,6 +331,8 @@ Partial Class Form1
         Me.Controls.Add(Me.RadioButton2)
         Me.Controls.Add(Me.RadioButton1)
         Me.Controls.Add(Me.Ordenación)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -317,4 +364,8 @@ Partial Class Form1
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
     Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
 End Class
--- p02/p02/Form1.vb
+++ p02/p02/Form1.vb
@@ -13,103 +13,120 @@
                 TextBox1.BackColor = Color.Red
                 Exit Sub
             End Try
+        Else
+            TextBox1.BackColor = Color.White
         End If
-        Try
+        If (TextBox2.Text <> "") Then
+            Try
+                Dim valor2 As Integer
+                valor2 = CInt(TextBox2.Text)
+                TextBox1.BackColor = Color.White
 
-            Dim valor2 As Integer
-            valor2 = CInt(TextBox2.Text)
+            Catch ex As Exception
+                MsgBox("El segundo textbox es un valro erroneo")
+                TextBox2.Focus()
+                TextBox2.BackColor = Color.Red
+                Exit Sub
+            End Try
+        Else
             TextBox2.BackColor = Color.White
+        End If
 
-        Catch ex As Exception
-            MsgBox("El segundo textbox es un valro erroneo")
-            TextBox2.Focus()
-            TextBox2.BackColor = Color.Red
-            Exit Sub
-        End Try
+        If (TextBox3.Text <> "") Then
+            Try
+                Dim valor3 As Integer
+                valor3 = CInt(TextBox3.Text)
+                TextBox3.BackColor = Color.White
 
-        Try
-
-            Dim valor3 As Integer
-            valor3 = CInt(TextBox3.Text)
+            Catch ex As Exception
+                MsgBox("El tercero textbox es un valro erroneo")
+                TextBox3.Focus()
+                TextBox3.BackColor = Color.Red
+                Exit Sub
+            End Try
+        Else
             TextBox3.BackColor = Color.White
+        End If
 
-        Catch ex As Exception
-            MsgBox("El tercer textbox es un valro erroneo")
-            TextBox3.Focus()
-            TextBox3.BackColor = Color.Red
-            Exit Sub
-        End Try
+        If (TextBox4.Text <> "") Then
+            Try
+                Dim valor4 As Integer
+                valor4 = CInt(TextBox4.Text)
+                TextBox4.BackColor = Color.White
 
-        Try
-
-            Dim valor4 As Integer
-            valor4 = CInt(TextBox4.Text)
+            Catch ex As Exception
+                MsgBox("El cuarto textbox es un valro erroneo")
+                TextBox4.Focus()
+                TextBox4.BackColor = Color.Red
+                Exit Sub
+            End Try
+        Else
             TextBox4.BackColor = Color.White
+        End If
 
+        If (TextBox5.Text <> "") Then
+            Try
+                Dim valor5 As Integer
+                valor5 = CInt(TextBox5.Text)
+                TextBox5.BackColor = Color.White
 
-        Catch ex As Exception
-            MsgBox("El cuarto textbox es un valro erroneo")
-            TextBox4.Focus()
-            TextBox4.BackColor = Color.Red
-            Exit Sub
-        End Try
-
-        Try
-
-            Dim valor5 As Integer
-            valor5 = CInt(TextBox5.Text)
+            Catch ex As Exception
+                MsgBox("El quinto textbox es un valro erroneo")
+                TextBox5.Focus()
+                TextBox5.BackColor = Color.Red
+                Exit Sub
+            End Try
+        Else
             TextBox5.BackColor = Color.White
+        End If
 
+        If (TextBox6.Text <> "") Then
+            Try
+                Dim valor6 As Integer
+                valor6 = CInt(TextBox6.Text)
+                TextBox6.BackColor = Color.White
 
-        Catch ex As Exception
-            MsgBox("El quinto textbox es un valro erroneo")
-            TextBox5.Focus()
-            TextBox5.BackColor = Color.Red
-            Exit Sub
-        End Try
-
-        Try
-
-            Dim valor6 As Integer
-            valor6 = CInt(TextBox6.Text)
+            Catch ex As Exception
+                MsgBox("El sexto textbox es un valro erroneo")
+                TextBox6.Focus()
+                TextBox6.BackColor = Color.Red
+                Exit Sub
+            End Try
+        Else
             TextBox6.BackColor = Color.White
+        End If
 
+        If (TextBox7.Text <> "") Then
+            Try
+                Dim valor7 As Integer
+                valor7 = CInt(TextBox7.Text)
+                TextBox7.BackColor = Color.White
 
-        Catch ex As Exception
-            MsgBox("El sexo textbox es un valro erroneo")
-            TextBox6.Focus()
-            TextBox6.BackColor = Color.Red
-            Exit Sub
-        End Try
-
-        Try
-
-            Dim valor7 As Integer
-            valor7 = CInt(TextBox7.Text)
+            Catch ex As Exception
+                MsgBox("El septimo textbox es un valro erroneo")
+                TextBox7.Focus()
+                TextBox7.BackColor = Color.Red
+                Exit Sub
+            End Try
+        Else
             TextBox7.BackColor = Color.White
+        End If
 
+        If (TextBox8.Text <> "") Then
+            Try
+                Dim valor8 As Integer
+                valor8 = CInt(TextBox8.Text)
+                TextBox8.BackColor = Color.White
 
-        Catch ex As Exception
-            MsgBox("El septimo textbox es un valro erroneo")
-            TextBox7.Focus()
-            TextBox7.BackColor = Color.Red
-            Exit Sub
-        End Try
-
-        Try
-
-            Dim valor8 As Integer
-            valor8 = CInt(TextBox8.Text)
+            Catch ex As Exception
+                MsgBox("El octavo textbox es un valro erroneo")
+                TextBox8.Focus()
+                TextBox8.BackColor = Color.Red
+                Exit Sub
+            End Try
+        Else
             TextBox8.BackColor = Color.White
-
-
-        Catch ex As Exception
-            MsgBox("El octavo textbox es un valro erroneo")
-            TextBox8.BackColor = Color.Red
-            TextBox8.Focus()
-            Exit Sub
-        End Try
-
+        End If
     End Sub
 
 
